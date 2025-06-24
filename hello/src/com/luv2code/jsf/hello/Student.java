60ac81a94a25da30c724e06f27c6677e367b61a3
Aluno <guilhermepinheiromoura1@gmail.com>
2025-06-24 12:39:50 -0300

    Criação do formulário first and last name

--- hello/src/com/luv2code/jsf/hello/Student.java
+++ hello/src/com/luv2code/jsf/hello/Student.java
@@ -1,5 +1,34 @@
 package com.luv2code.jsf.hello;
 
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
 public class Student {
 
+	private String firstName;
+	private String lastName;
+	
+	
+	//construtor vazio
+	public Student() {
+		
+	}
+
+	//getters and setters
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
 }
