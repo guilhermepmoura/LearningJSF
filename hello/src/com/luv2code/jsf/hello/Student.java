31cb2299f6ff7bced82f146328edfb7c9e2ef222
Aluno <guilhermepinheiromoura1@gmail.com>
2025-06-24 13:16:11 -0300

    Criando uma opção para escolher seu país dentro do formulário.

--- hello/src/com/luv2code/jsf/hello/Student.java
+++ hello/src/com/luv2code/jsf/hello/Student.java
@@ -7,6 +7,7 @@ public class Student {
 
 	private String firstName;
 	private String lastName;
+	private String country;
 	
 	
 	//construtor vazio
@@ -30,5 +31,14 @@ public class Student {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
 	
 }
